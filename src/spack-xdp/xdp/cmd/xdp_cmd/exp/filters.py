81e3fecc630066f3a69ff0960907b1539f35cec2
Eduardo Rothe <eduardo.rothe@epfl.ch>
2023-07-21 18:52:01 +0200

    Add new functions for filter problem

--- src/spack-xdp/xdp/cmd/xdp_cmd/exp/filters.py
+++ src/spack-xdp/xdp/cmd/xdp_cmd/exp/filters.py
@@ -1,41 +1,18 @@
-import json
-from pdb import set_trace as st
-
-filters = {"gpu": "nvidia", "mpi": "infiniband"}
-
 # 1 filter for variants
-pkg1 = {"metallo": {"variants": {"common": "~int64 +double +hdf5 +metis +mpi +superlu-dist "
-                                           "+hypre +suite-sparse",
-                                 "gpu": {"nvidia": "+cuda cuda_arch=<cuda_arch>",
+pkg1 = {"bizarro": {"variants": {"common": "+sci +experiments",
+                                 "gpu": {"nvidia": "+cuda",
                                          "none": "~cuda"}
                                },
-                  "dependencies": {"common": ["hdf5 ~ipo +mpi +szip +hl +fortran +cxx",
-                                              "spec2"],
-                                   "gpu": {"nvidia": "+cuda cuda_arch=<cuda_arch>",
-                                           "none": "~cuda"}
+                  "dependencies": {"common": ["hdf5 +mpi", "libtiff"],
+                                   "gpu": {"nvidia": "pkg +cuda",
+                                           "none": "pkg ~cuda"}
                                    }
                   }
         }
 
-# 2 filters for variants
-pkg2 = {"metallo": {"variants": {"common": "~int64 +double +hdf5 +metis +mpi +superlu-dist "
-                                           "+hypre +suite-sparse",
-                                 "gpu": {"nvidia": "+cuda cuda_arch=<cuda_arch>",
-                                         "none": "~cuda"},
-                                 "mpi": {"infiniband": "+mpi",
-                                         "ethernet": "none"}
-
-                               },
-                  "dependencies": {"common": ["hdf5 ~ipo +mpi +szip +hl +fortran +cxx",
-                                              "spec2"],
-                                   "gpu": {"nvidia": "+cuda cuda_arch=<cuda_arch>",
-                                           "none": "~cuda"}
-                                   }
-                  }
-        }
 
 # 0 filters fro variants
-pkg3 = {"metallo": {"variants": {"common": "~int64 +double +hdf5 +metis +mpi +superlu-dist "
+pkg2 = {"metallo": {"variants": {"common": "~int64 +double +hdf5 +metis +mpi +superlu-dist "
                                            "+hypre +suite-sparse",
                                  },
                   "dependencies": {"common": ["hdf5 ~ipo +mpi +szip +hl +fortran +cxx",
@@ -46,30 +23,83 @@ pkg3 = {"metallo": {"variants": {"common": "~int64 +double +hdf5 +metis +mpi +su
                   }
         }
 
-# Which filters are declared in package ?
-# The answer is the subset of filters declared in platform which are also
-# used in the package definition.
-def fip(fltrs, pkg):
-    """Returns list of filters used in package"""
-    return [ k for k in fltrs.keys() if k in pkg ]
+# 2 filters for variants
+pkg3 = {"metallo": {"variants": {"common": "+radiation +cyborg",
+                                 "gpu": {"nvidia": "+inteligence",
+                                         "none": "~cuda"},
+                                 "mpi": {"infiniband": "+speed",
+                                         "ethernet": "none"}
+                               },
+                  "dependencies": {"common": ["green", "kryptonite"],
+                                   "gpu": {"nvidia": "+morph",
+                                           "none": "~cuda"}
+                                   }
+                  }
+        }
 
-# Which values are declared in package for given filters ?
-def fv(fltrs, pkg):
-    """Returns filter value"""
-    st()
-    return fltrs[fip(fltrs,v)[0]]
+odts = {'gpu': 'nvidia', 'mpi': 'infiniband'}
 
-v = pkg1["metallo"]["variants"]
-print(fip(filters, v))
-print(fv(filters, v))
+# which platform ODITIES concern this package ?
+def package_odities(plat_odts: dict, pkg_varts: dict) -> list:
+    """Returns list of platform odities used by package.
 
-v = pkg2["metallo"]["variants"]
-print(fip(filters, v))
-print(fv(filters, v))
+    plat_odts - platform odities as defined in platform file.
+    pkg_varts - variants in package common to all platforms.
 
-v = pkg3["metallo"]["variants"]
-print(fip(filters, v))
-print(fv(filters, v))
+          plat_odts: {'gpu': 'nvidia', 'mpi': 'infiniband'}
+          pkg_varts: {'common': '+sci +exp', 'gpu': {'nvidia': '+cuda', 'amd': '+rocm'}
+          return value: ['gpu']
+    """
+    return [ k for k in plat_odts.keys() if k in pkg_varts ]
 
-st()
+# what are the platform RECOMMENDATIONS for this package ?
+def platform_recommendation(plat_odts: dict, pkg_odts: list) -> list:
+    """Returns the platform recomendation for the odities declared by the package.
+
+    This function will only return the values specified in the platform dictionary
+    for which the key was also declared in the package
+
+          plat_odts: {'gpu': 'nvidia', 'mpi': 'infiniband'}
+          pkg_odts: ['gpu', `mpi`]
+          return value:  ['nvidia', 'infiniband']
+    """
+    return [ plat_odts[v] for v in pkg_odts ]
+
+# which are the package variants given the platform odities ?
+def odts_variants(plat_odts: dict, pkg_varts: dict) -> list:
+    """Returns the package variants for the platform odities.
+
+          plat_odts - platform odities as defined in platform file.
+          pkg_varts - variants in package common to all platforms.
+
+    """
+    odts = package_odities(plat_odts, pkg_varts)
+    recs = platform_recommendation(plat_odts, odts)
+    return [ pkg_varts[odt][rec] for odt, rec in zip(odts, recs) ]
+
+
+def print_case(pkg):
+    """Print debug info"""
+
+    class Variant(dict):
+        def get2(self, key):
+            """Return values from subdict in single key dict"""
+            k = list(self.keys())[0]
+            return self[k][key]
+
+    v = Variant(pkg).get2('variants')
+    print(f'pkg variants: {v}')
+    print(f'package_odities: {package_odities(odts, v)}')
+    print(f'platform_recommendations: {platform_recommendation (odts, package_odities(odts, v))}')
+    print(f'odity variants: {odts_variants(odts, v)}')
+
+# Initial data
+print(f'platform odts: {odts}')
+print()
+print_case(pkg1)
+print()
+print_case(pkg2)
+print()
+print_case(pkg3)
+print()
 
